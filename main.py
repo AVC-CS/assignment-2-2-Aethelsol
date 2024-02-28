3b9f863aacb7c7ebb91963ecb8c9f56f577dbb58
Aethelsol <157772809+Aethelsol@users.noreply.github.com>
2024-02-28 03:23:43 +0000

    done

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ def main():
     Use the same variables: celsius fahrenheit 
     ##################################################
     """
+    celsius = int(input())
+    fahrenheit = 9/5 * (celsius) + 32
 
+    print (fahrenheit)
     """
     ########################################
     # Do not delete the return statement
